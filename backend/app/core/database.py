49a2a522b70d9258627d46a9d8cc38d6a03d4938
Radika Badragtiha <puturadika69@gmail.com>
2025-12-24 14:10:55 +0800

    database connection and update requrements.txt

---
+++ backend/app/core/database.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+
+client: AsyncIOMotorClient | None = None
+
+async def connect_to_mongo():
+    global client
+    client = AsyncIOMotorClient(settings.MONGODB_URI)
+
+async def close_mongo_connection():
+    client.close()
+
+def get_database():
+    return client[settings.MONGODB_DB]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.core.database import connect_to_mongo, close_mongo_connection
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_to_mongo()
+    yield
+    await close_mongo_connection()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
